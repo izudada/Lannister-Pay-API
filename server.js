a3b5b533f7cb5a8b77ba6d15d37f05c9c1fc59fd
Anthony <tonyudeagbala@gmail.com>
2022-07-03 11:37:06 +0100

    Edit server.js

--- server.js
+++ server.js
@@ -8,8 +8,4 @@ const paymentRoutes = require('./routes/payments')
 app.use('/split-payments', paymentRoutes)
 
 
-//  For development server or localhost
-// app.listen(3000, () => console.log('Server has started'))
-
-//  For production or deployment server
-app.listen()
+app.listen(3000, () => console.log('Server has started'))
