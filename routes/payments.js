456ef0a4cd545f076b863c4099f8d04854735ac5
Anthony <tonyudeagbala@gmail.com>
2022-07-03 10:41:39 +0100

    Add generic comment

--- routes/payments.js
+++ routes/payments.js
@@ -5,6 +5,14 @@ module.exports = router
 
 
 function flat(splitInfo, balance, result) {
+    /*
+        * @desc computes data for all FLAT types
+        * @param int $splitCount - loop counter or state.
+        * @param Obj $splitInfo - All split info.
+        * @param int $balance - Initial balance.
+        * @param array $result - an array to sotre individual SplitBreakdown.
+    */
+   
     for(let splitCount = 0; splitCount < splitInfo.length; splitCount++) {
         if (splitInfo[splitCount].SplitType == 'FLAT') {
             result.push({
@@ -18,6 +26,15 @@ function flat(splitInfo, balance, result) {
 }
 
 function percentage(splitInfo, balance, result) {
+    /*
+        * @desc computes data for all PERCENTAGE types
+        * @param int $splitCount - loop counter or state.
+        * @param Obj $splitInfo - All split info.
+        * @param int $balance - Initial balance.
+        * @param array $result - an array to sotre individual SplitBreakdown.
+        * @param int $splitValue - result of calc the percentage of a SplitValue.
+    */
+
     for(let splitCount = 0; splitCount < splitInfo.length; splitCount++) {
         if (splitInfo[splitCount].SplitType == 'PERCENTAGE') {
             let splitValue = (splitInfo[splitCount].SplitValue * balance) / 100
@@ -32,6 +49,18 @@ function percentage(splitInfo, balance, result) {
 }
 
 function ratio(splitInfo, balance, result) {
+
+    /*
+        * @desc computes data for all RATIO types.
+        * @param int $totalRatio - Total of all the ration SplitValue.
+        * @param int $ratioBalance - static balance for calculating ratio.
+        * @param int $splitCount - loop counter or state.
+        * @param Obj $splitInfo - All split info.
+        * @param int $balance - Initial balance.
+        * @param array $result - an array to sotre individual SplitBreakdown.
+        * @param int $amount - result of calc the ratio of a SplitValue.
+    */
+
     let totalRatio = 0
     let ratioBalance = balance
     for(let splitCount = 0; splitCount < splitInfo.length; splitCount++) {
@@ -54,9 +83,13 @@ function ratio(splitInfo, balance, result) {
 }
 
 router.post('/compute', (req, res) => {
+
+    /*
+        * @desc An endpoint to compute split information.
+    */
+
     const id = req.body.ID
     let amount = req.body.Amount
-    const currency = req.body.Currency
     const splitInfo = req.body.SplitInfo
     var balance = req.body.Amount
     var result = []
